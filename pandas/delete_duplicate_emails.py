09207f82149dd6726b8ba62531064b11ff094fd8
williamj-owens <william.owens93@gmail.com>
2024-01-13 02:21:34 -0800

    Add pandas solution

---
+++ pandas/delete_duplicate_emails.py
@@ -0,0 +1,69 @@
+"""
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+
+For SQL users, please note that you are supposed to write a DELETE statement and not a SELECT one.
+
+For Pandas users, please note that you are supposed to modify Person in place.
+
+After running your script, the answer shown is the Person table. The driver will first compile and run your piece of code and then show the Person table. The final order of the Person table does not matter.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Person table:
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Output: 
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+Explanation: john@example.com is repeated two times. We keep the row with the smallest Id = 1.
+"""
+
+import pandas as pd
+
+# Create person DataFrame
+person = pd.DataFrame({
+    "id": [1, 2, 3],
+    "email": ["john@example.com", "bob@example.com", "john@example.com"]
+})
+
+# View person DataFrame
+person
+
+# Function to delete duplicate emails and keep email with smallest 'id'
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    # Sort the DataFrame by 'id'
+    person.sort_values(by='id', inplace=True)
+    # Drop the duplicate emails and keep first
+    person.drop_duplicates(subset='email', keep='first', inplace=True)
+
+# Call the function
+delete_duplicate_emails(person)
+
+# View DataFrame after duplicate deletion
+person
