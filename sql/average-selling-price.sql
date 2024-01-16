d28d4ee91981ef5fa4ff779e3ab7dcf4f9bef46b
williamjowens <125225331+williamjowens@users.noreply.github.com>
2024-01-15 23:09:07 -0800

    Edit solution

--- sql/average-selling-price.sql
+++ sql/average-selling-price.sql
@@ -25,13 +25,16 @@ INSERT INTO UnitsSold (product_id, purchase_date, units) VALUES
 (2, '2019-03-22', 30);
 
 SELECT
-    u.product_id,
-    ROUND(SUM(p.price * u.units) / SUM(u.units), 2) AS average_price
+    p.product_id,
+    COALESCE(
+        ROUND(
+            SUM(p.price * u.units) / SUM(u.units), 2), 0)
+            AS average_price
 FROM
-    UnitsSold u
-JOIN
-    Prices p ON u.product_id = p.product_id
-WHERE
-    u.purchase_date BETWEEN p.start_date AND p.end_date
-GROUP BY
-    u.product_id;
+    Prices p 
+LEFT JOIN 
+    UnitsSold u 
+    ON p.product_id = u.product_id 
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date 
+GROUP BY 
+    p.product_id;
