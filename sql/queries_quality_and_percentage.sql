b0956758bbb16d7ab5d1efb2081764e8cf227cc8
williamj-owens <william.owens93@gmail.com>
2024-03-15 18:13:08 -0700

    Add SQL Solution

---
+++ sql/queries_quality_and_percentage.sql
@@ -0,0 +1,60 @@
+"""
+Table: Queries
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| query_name  | varchar |
+| result      | varchar |
+| position    | int     |
+| rating      | int     |
++-------------+---------+
+This table may have duplicate rows.
+This table contains information collected from some queries on a database.
+The position column has a value from 1 to 500.
+The rating column has a value from 1 to 5. Query with rating less than 3 is a poor query.
+ 
+
+We define query quality as:
+
+The average of the ratio between query rating and its position.
+
+We also define poor query percentage as:
+
+The percentage of all queries with rating less than 3.
+
+Write a solution to find each query_name, the quality and poor_query_percentage.
+
+Both quality and poor_query_percentage should be rounded to 2 decimal places.
+
+Return the result table in any order.
+
+The result format is in the following example.
+"""
+
+CREATE TABLE Queries (
+    query_name VARCHAR(255),
+    result VARCHAR(255),
+    position INT,
+    rating INT
+);
+
+INSERT INTO Queries (query_name, result, position, rating) VALUES
+('Dog', 'Golden Retriever', 1, 5),
+('Dog', 'German Shepherd', 2, 5),
+('Dog', 'Mule', 200, 1),
+('Cat', 'Shirazi', 5, 2),
+('Cat', 'Siamese', 3, 3),
+('Cat', 'Sphynx', 7, 4);
+
+
+SELECT 
+    query_name,
+    ROUND(AVG(rating / position), 2) as quality,
+    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / COUNT(*), 2) AS poor_query_percentage 
+FROM 
+    Queries 
+WHERE 
+    query_name IS NOT NULL 
+GROUP BY 
+    query_name;
