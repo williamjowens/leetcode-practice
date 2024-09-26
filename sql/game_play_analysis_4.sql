03a043b512d31bb6e75c8a9116d18dfd39a63920
williamj-owens <william.owens93@gmail.com>
2024-09-26 00:27:30 -0700

    Add sql solution

---
+++ sql/game_play_analysis_4.sql
@@ -0,0 +1,81 @@
+"""
+Table: Activity
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key (combination of columns with unique values) of this table.
+This table shows the activity of players of some games.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+ 
+
+Write a solution to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. In other words, you need to count the number of players that logged in for at least two consecutive days starting from their first login date, then divide that number by the total number of players.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Activity table:
++-----------+-----------+------------+--------------+
+| player_id | device_id | event_date | games_played |
++-----------+-----------+------------+--------------+
+| 1         | 2         | 2016-03-01 | 5            |
+| 1         | 2         | 2016-03-02 | 6            |
+| 2         | 3         | 2017-06-25 | 1            |
+| 3         | 1         | 2016-03-02 | 0            |
+| 3         | 4         | 2018-07-03 | 5            |
++-----------+-----------+------------+--------------+
+Output: 
++-----------+
+| fraction  |
++-----------+
+| 0.33      |
++-----------+
+Explanation: 
+Only the player with id 1 logged back in after the first day he had logged in so the answer is 1/3 = 0.33
+"""
+
+CREATE TABLE Activity (
+    player_id INT,
+    device_id INT,
+    event_date DATE,
+    games_played INT,
+    PRIMARY KEY (player_id, event_date)
+);
+
+INSERT INTO Activity (player_id, device_id, event_date, games_played) VALUES 
+(1, 2, '2016-03-01', 5),
+(1, 2, '2016-03-02', 6),
+(2, 3, '2017-06-25', 1),
+(3, 1, '2016-03-02', 0),
+(3, 4, '2018-07-03', 5);
+
+
+WITH first_login AS (
+    SELECT 
+        player_id, 
+        MIN(event_date) AS first_date
+    FROM 
+        Activity
+    GROUP BY 
+        player_id
+)
+SELECT 
+    ROUND(
+        1.0 * COUNT(f.player_id) / (SELECT COUNT(*) FROM first_login),
+        2
+    ) AS fraction
+FROM 
+    first_login f
+JOIN 
+    Activity a
+    ON f.player_id = a.player_id
+    AND a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY)
